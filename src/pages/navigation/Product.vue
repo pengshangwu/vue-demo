9b783b529f7d414c2b2b74f9c25303542181990e
pengpeng <peng.peng@iac.com.tw>
2020-03-02 13:54:45 +0800

    vuex的demo

--- src/pages/navigation/Product.vue
+++ src/pages/navigation/Product.vue
@@ -1,14 +1,62 @@
 <template>
   <div>
-    <h1>Product</h1>
-    <h1>{{count}}</h1>
-    <h1>{{address}}</h1>
-    <h1>{{name}}</h1>
+    <h1>1、state/mapState demo</h1>
+    <h2>1.1、mapState</h2>
+    <h3>{{count}}</h3>
+    <h3>{{address}}</h3>
+    <h3>{{name}}</h3>
+    <h3>1.2、state</h3>
+    <h3>{{this.$store.state.count}}</h3>
+
+    <h1>2、getter/mapGetters demo</h1>
+    <h2>2.1、getter/todo is true</h2>
+    <h3>{{this.$store.state.todos.length}}</h3>
+
+    <h2>2.2、mapGetters/todo is true</h2>
+    <ul>
+      <li v-for="item in getTodosTrue" :key="item.id">
+        <span>{{item.id}}</span>
+        <span>{{item.done}}</span>
+        <span>{{item.text}}</span>
+      </li>
+    </ul>
+
+    <h2>2.3、mapGetters/todo is false</h2>
+    <li v-for="item in getTodosFalse" :key="item.id">
+      <span>{{item.id}}</span>
+      <span>{{item.done}}</span>
+      <span>{{item.text}}</span>
+    </li>
+
+    <h1>3、mutation/mapMutations demo</h1>
+    <h2>3.1、mutation</h2>
+    <button @click="update">更新地址</button>
+
+    <h2>3.2、mapMutations</h2>
+    <button @click="increate">增加</button>
+    <button @click="decrease">减少</button>
+    <button @click="updateName({'name': 'hello '})">修正名字</button>
+    <button @click="updateAddress({'address': 'jiangxi '})">修正地址</button>
+    <h3>{{address}}</h3>
+    <h3>{{name}}</h3>
+
+    <h1>4、actions/mapActions demo</h1>
+    <h2>4.1、actions</h2>
+
+    <h2>4.2、mapActions</h2>
+    <button @click="Aincreate">增加</button>
+    <button @click="Adecrease">减少</button>
+    <h3>{{count}}</h3>
+
+
+
+
+    <button @click="Sincreate">strict模式</button>
   </div>
 </template>
 
 <script>
-  import { mapState } from 'vuex'
+  import { mapState, mapGetters, mapMutations, mapActions } from 'vuex'
   export default {
     name: "Product",
     computed: {
@@ -16,7 +64,29 @@
         'count',
         'name',
         'address'
-      ])
+      ]),
+      ...mapGetters([
+        'getTodosTrue',
+        'getTodosFalse'
+      ]),
+    },
+    methods: {
+      ...mapActions([
+        'Aincreate',
+        'Adecrease',
+      ]),
+      ...mapMutations([
+        'increate',
+        'decrease',
+        'updateName',
+        'updateAddress'
+      ]),
+      update() {
+        this.$store.commit('updateAddress', {'address': 'jiangxi '})
+      },
+      Sincreate() {
+        this.$store.state.count++
+      }
     },
   }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,17 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex);
 
+// 监听调用mutation中type为increate函数，调用成功就触发如下操作
+const myPlugin = store => {
+  // mutation 的格式为：{ type, payload }，通过这个对象可以判断是哪一个 mutation 被调用了，额外的参数是什么。
+  store.subscribe((mutation, state) => {
+      if (mutation.type === 'increate') {
+          console.log(mutation)
+          console.log(state)
+      }
+  })
+}
+
 // https://www.cnblogs.com/m2maomao/p/9954640.html
 //一个store  , Vuex.store的 实例
 const store = new Vuex.Store({
@@ -10,30 +21,60 @@ const store = new Vuex.Store({
     count : 1,
     name: 'psw',
     address: 'zhangshu',
+    todos: [
+      {id: 1, done: true, text: '我是码农'},
+      {id: 2, done: false, text: '我是码农201号'},
+      {id: 3, done: true, text: '我是码农202号'}
+    ]
   },
   getters:{            //  Getter相当于vue中的computed计算属性
-    getStateCount: function(state) {
-      return state.count + 1;
+    getTodosTrue: (state) => {
+      return state.todos.filter(item => item.done === true)
+    },
+    getTodosFalse: (state) => {
+      return state.todos.filter(item => item.done === false)
     }
   },
   mutations: {
-    addition(state) {
-      state.count = state.count + 1;
+    increate(state) {
+      state.count++;
     },
-    reduction(state) {
-      state.count = state.count - 1;
+    decrease(state) {
+      state.count--;
+    },
+    updateName(state, payload) {
+      state.name = payload.name + state.name  
+    },
+    updateAddress(state, payload) {
+      state.address = payload.address + state.address  
     }
+    // store.commit({
+    //   type: 'increment',
+    //   amount: 10
+    // })
   },
+  /**
+   * commit 和 dispatch的区别
+   * https://shq5785.blog.csdn.net/article/details/100763161
+   */
   actions: {      //注册actions，类似vue里面的methods
     //通过这个修改state里面的值
     // 可以直接用mutations修改，但是官方建议使用actions去调用mutations去修改
-    addition(context) {
-      context.commit("addition");
+    Aincreate({ commit }) {
+      // setTimeout(() => {
+      //   commit("increate");
+      // }, 1000000)
+      commit("increate");//同步操作，数据提交至 mutations ，可用于登录成功后读取用户信息写到缓存里
     },
-    reduction(context){
-      context.commit("reduction");
-    }
-  }
+    Adecrease({dispatch}) {
+      setTimeout(() => {
+        dispatch({type: "Aincreate"});// 数据提交至 actions ，可用于向后台提交数据
+      }, 2000)
+    },
+  },
+  plugins: [myPlugin],
+  strict: true  // 不允许在mutations之外修改state中的值
 })
+
 
 export default store
